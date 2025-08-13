5274c341ed98f4b3ca73faccb80bc1da3103f6b4
Gentleelephant <1132960613@qq.com>
2025-08-13 09:38:44 +0800

    remove nolock option from NFS mount in fluid_config_init.py
    Signed-off-by: Gentleelephant <1132960613@qq.com>

--- addons/nfs/docker/fluid_config_init.py
+++ addons/nfs/docker/fluid_config_init.py
@@ -31,7 +31,8 @@ MNT_TO=$targetPath
 
 trap "umount ${MNT_TO}" SIGTERM
 mkdir -p ${MNT_TO}
-mount -t nfs ${MNT_FROM} ${MNT_TO} -o ${MOUNT_OPTIONS},nolock
+mount -t nfs ${MNT_FROM} ${MNT_TO}
+# mount -t nfs ${MNT_FROM} ${MNT_TO} -o ${MOUNT_OPTIONS},nolock
 sleep inf
 """
 
