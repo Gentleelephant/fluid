be6e9237d82bc58643611dfa1fdfb5b7b9871d43
Gentleelephant <1132960613@qq.com>
2026-06-12 17:27:03 +0800

    refactor(docker): restructure NFS mount script generation logic
    - Extract mount options parsing and formatting into separate functions
    - Merge mount options from environment, runtime, and config sources
    - Generate mount script dynamically with conditional mount options handling
    - Replace inline script string with assembled lines including shell quoting
    - Encapsulate main logic into main() function for clarity and reuse
    - Improve readability and maintainability of fluid_config_init.py script

--- addons/nfs/docker/fluid_config_init.py
+++ addons/nfs/docker/fluid_config_init.py
@@ -15,14 +15,56 @@
 #!/usr/bin/env python
 
 import json
+import os
 
-rawStr = ""
-with open("/etc/fluid/config/config.json", "r") as f:
-    rawStr = f.readlines()
 
-rawStr = rawStr[0]
+def shell_quote(value):
+    return "'" + value.replace("'", "'\\''") + "'"
 
-script = """
+
+def parse_mount_options(options_text):
+    options = {}
+    if not options_text:
+        return options
+    for item in options_text.split(','):
+        item = item.strip()
+        if not item:
+            continue
+        if '=' in item:
+            key, value = item.split('=', 1)
+            options[key.strip()] = value.strip()
+        else:
+            options[item] = ''
+    return options
+
+
+def format_mount_options(options):
+    items = []
+    for key in sorted(options.keys()):
+        value = options[key]
+        if value:
+            items.append("%s=%s" % (key, value))
+        else:
+            items.append(key)
+    return ",".join(items)
+
+
+def merge_options(*option_maps):
+    merged = {}
+    for options in option_maps:
+        merged.update(options or {})
+    return merged
+
+
+def generate_mount_script(obj):
+    mount = obj['mounts'][0]
+    config_options = mount.get('options', {})
+    runtime_options = obj.get('runtimeOptions', {})
+    env_options = parse_mount_options(os.environ.get('MOUNT_OPTIONS', ''))
+    merged_options = merge_options(env_options, runtime_options, config_options)
+    formatted_options = format_mount_options(merged_options)
+
+    script = """
 #!/bin/bash
 set -ex
 MNT_FROM=$mountPoint
@@ -31,13 +73,31 @@ MNT_TO=$targetPath
 
 trap "umount ${MNT_TO}" SIGTERM
 mkdir -p ${MNT_TO}
-mount -t nfs ${MNT_FROM} ${MNT_TO} -o ${MOUNT_OPTIONS}
+if [ -n "$MOUNT_OPTIONS" ]; then
+    mount -t nfs ${MNT_FROM} ${MNT_TO} -o ${MOUNT_OPTIONS}
+else
+    mount -t nfs ${MNT_FROM} ${MNT_TO}
+fi
 sleep inf
 """
 
-obj = json.loads(rawStr)
+    lines = [
+        "mountPoint=\"%s\"\n" % mount['mountPoint'],
+        "targetPath=\"%s\"\n" % obj['targetPath'],
+    ]
+    if formatted_options:
+        lines.append("MOUNT_OPTIONS=%s\n" % shell_quote(formatted_options))
+    lines.append(script)
+    return "".join(lines)
 
-with open("mount-nfs.sh", "w") as f:
-    f.write("mountPoint=\"%s\"\n" % obj['mounts'][0]['mountPoint'])
-    f.write("targetPath=\"%s\"\n" % obj['targetPath'])
-    f.write(script)
+
+def main():
+    with open("/etc/fluid/config/config.json", "r") as f:
+        rawStr = f.readlines()
+    obj = json.loads(rawStr[0])
+    with open("mount-nfs.sh", "w") as f:
+        f.write(generate_mount_script(obj))
+
+
+if __name__ == "__main__":
+    main()
